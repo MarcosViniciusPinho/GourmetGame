e702171f019e9f6b13718f5980bfe4490437ee4b
Marcos Pinho <marcosjava2008@gmail.com>
2023-04-21 03:49:02 -0300

    feat: Upgrading base project structure

---
+++ application/src/main/java/com/objective/Start.java
@@ -0,0 +1,10 @@
+package com.objective;
+
+import javax.swing.JOptionPane;
+
+public class Start {
+    public static void main(String[] args) {
+//        System.out.println("Olá mundo!");
+        JOptionPane.showMessageDialog(null, "Olá mundo!");
+    }
+}
