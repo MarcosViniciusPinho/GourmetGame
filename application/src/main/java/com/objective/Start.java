6c11c87be73e827729df6658d34277c679f7692a
Marcos Pinho <marcosjava2008@gmail.com>
2023-04-21 13:04:25 -0300

    refactor: Refactoring start of swing screens

--- application/src/main/java/com/objective/Start.java
+++ application/src/main/java/com/objective/Start.java
@@ -1,10 +1,57 @@
 package com.objective;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class Start {
+
+    private void init() {
+        JOptionPane.showMessageDialog(
+                null,
+                "Pense em um prato que gosta",
+                "Jogo Gourmet",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+    }
+
+    private int getSelectedResultPasta() {
+        return JOptionPane.showConfirmDialog(
+                null,
+                "O prato que você pensou é massa?",
+                "Confirm",
+                JOptionPane.YES_NO_OPTION
+        );
+    }
+
+    private int getSelectedResultLasagna() {
+        return JOptionPane.showConfirmDialog(
+                null,
+                "O prato que você pensou é Lasanha?",
+                "Confirm",
+                JOptionPane.YES_NO_OPTION
+        );
+    }
+
+    private void applyContextLasagna() {
+        var result = this.getSelectedResultLasagna();
+        if (result == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "Acertei de novo!");
+            this.applyContext();
+        } else if (result == JOptionPane.NO_OPTION) {
+            JOptionPane.showMessageDialog(null, "Você clicou em Não. - Lasanha");
+        }
+    }
+
+    public void applyContext() {
+        this.init();
+        var result = this.getSelectedResultPasta();
+        if (result == JOptionPane.YES_OPTION) {
+            this.applyContextLasagna();
+        } else if (result == JOptionPane.NO_OPTION) {
+            JOptionPane.showMessageDialog(null, "Você clicou em Não. - Massa");
+        }
+    }
+
     public static void main(String[] args) {
-//        System.out.println("Olá mundo!");
-        JOptionPane.showMessageDialog(null, "Olá mundo!");
+        new Start().applyContext();
     }
 }
